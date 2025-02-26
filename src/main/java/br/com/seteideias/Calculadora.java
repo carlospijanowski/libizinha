b270a28b4934d78e849d084e93ddb3cdd727ceec
Cartaxo <carlos.pijanowski@gmail.com>
2025-02-25 21:03:33 -0300

    teste com nova impl

--- src/main/java/br/com/seteideias/Calculadora.java
+++ src/main/java/br/com/seteideias/Calculadora.java
@@ -8,4 +8,9 @@ public class Calculadora {
     public int soma(int a, int b, int c) {
         return a + b + c;
     }
+
+    public int soma(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
 }
